bca09b6d26214b00ef04460cd9e0ced865d0b321
huideyeren <takakura.yusuke@gmail.com>
2021-09-03 20:53:31 +0900

    画像回転

---
+++ rotate.py
@@ -0,0 +1,28 @@
+import glob
+import os
+
+from PIL import Image
+
+def rotate(path):
+    image = Image.open(path)
+    rotated_image = image.rotate(90, expand=True)
+    return rotated_image
+
+def bundle_rotate(dir):
+    path_list = glob.glob(dir + '\*')
+    name_list = []
+    ext_list = []
+
+    for i in path_list:
+        file = os.path.basename(i)
+        name, ext = os.path.splitext(file)
+        name_list.append(name)
+        ext_list.append(ext)
+
+        out_path = os.path.join(*[dir, name + '_resize' + ext])
+
+        img = rotate(i)
+        img.save(out_path)
+    return
+
+bundle_rotate('./images')
